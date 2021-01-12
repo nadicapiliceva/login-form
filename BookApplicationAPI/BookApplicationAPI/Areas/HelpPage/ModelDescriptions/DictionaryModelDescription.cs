11036db58c90d13815823e98a8b196be7e84d1bc
Nadica Piliceva <nadica_piliceva@hotmail.com>
2021-01-12 22:57:57 +0100

    Commit of the code

---
+++ BookApplicationAPI/BookApplicationAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BookApplicationAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BookApplicationAPI/BookApplicationAPI/Models/IdentityModels.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BookApplicationAPI.Models
+{
+    public class ApplicationUser: IdentityUser
+    { 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("BookApplicationDb", throwIfV1Schema: false)
+        {
+        }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //AspNetUsers -> User
+            modelBuilder.Entity<ApplicationUser>()
+                .ToTable("User");
+            //AspNetRoles -> Role
+            modelBuilder.Entity<IdentityRole>()
+                .ToTable("Roles");
+            //AspNetUserRoles -> UserRole
+            modelBuilder.Entity<IdentityUserRole>()
+                .ToTable("UserRoles");
+            //AspNetUserClaims -> UserClaim
+            modelBuilder.Entity<IdentityUserClaim>()
+                .ToTable("UserClaim");
+            //AspNetUserLogins -> UserLogin
+            modelBuilder.Entity<IdentityUserLogin>()
+                .ToTable("UserLogin");
+        }
+
+    }
+}
